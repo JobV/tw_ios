f1d5dce75875d9c169a9083d51ec8bb6b6cf174f
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-12 23:09:49 +0000

    xcode project init

---
+++ therewhere/therewhere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  therewhere
+//
+//  Created by Marcelo Lebre on 12/12/14.
+//  Copyright (c) 2014 therewhereinc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
