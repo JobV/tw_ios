2f1cf6f9f4608c588d8f614dcf7acacc37cd3c1d
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-14 16:12:41 +0000

    startin login view and controller

--- therewhere/therewhere/LoginViewController.swift
+++ therewhere/therewhere/LoginViewController.swift
@@ -1,25 +1,28 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  therewhere
 //
-//  Created by Marcelo Lebre on 12/12/14.
+//  Created by Marcelo Lebre on 14/12/14.
 //  Copyright (c) 2014 therewhereinc. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoPinImage: UIImageView!
+    // define the class
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func login(sender: AnyObject) {
+        
+    }
 }
-
