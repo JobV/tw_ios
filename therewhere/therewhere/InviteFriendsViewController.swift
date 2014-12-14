04c1f9ee2137d3b0b94e4bf17cb3cb2d1b39ec71
Marcelo Lebre <marcelo@dwitmee.com>
2014-12-14 23:12:29 +0000

    adding friendsview tableview and customcellview

--- therewhere/therewhere/InviteFriendsViewController.swift
+++ therewhere/therewhere/InviteFriendsViewController.swift
@@ -9,44 +9,61 @@
 import Foundation
 import UIKit
 
-class InviteFriendsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var addSomeFreindsLabel: UILabel!
-
-    @IBOutlet weak var nextButton: UIButton!
-
-    @IBAction func next(sender: UIButton) {
+class CustomTableViewCell : UITableViewCell {
+    @IBOutlet var backgroundImage: UIImageView?
+    @IBOutlet var titleLabel: UILabel?
+    
+    func loadItem(#title: String, image: String) {
+        backgroundImage?.image = UIImage(named: "Logo Pin" )
+        titleLabel?.text = title
     }
+}
+
+class InviteFriendsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
     @IBOutlet
     var tableView: UITableView!
-    var items: [String] = ["We", "Heart", "Swift"]
-    // define the class
+    var items: [(String, String)] = [
+        ("Marcelo Lebre", "Logo Pin"),
+        ("Job van der Voort", "Logo Pin"),
+        ("Carla Matos", "Logo Pin"),
+        ("Rita Loureiro", "Logo Pin"),
+        ("Maçã Rocks", "Logo Pin")
+    ]
+    
+    @IBAction func next(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var nib = UINib(nibName: "CustomTableViewCell", bundle: nil)
+        
+        tableView.registerNib(nib, forCellReuseIdentifier: "customCell")
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
-    }
-    
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        println("You selected cell #\(indexPath.row)!")
+        return self.items.count;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        var cell:CustomTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("customCell") as CustomTableViewCell
         
-        cell.textLabel?.text = self.items[indexPath.row]
+        // this is how you extract values from a tuple
+        var (title, image) = items[indexPath.row]
+        
+        cell.loadItem(title: title, image: image)
         
         return cell
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
+
+        
 }
