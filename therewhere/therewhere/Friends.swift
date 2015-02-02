1de4c7168991bedf8fca05e7e6a2dc85f2563446
Marcelo Lebre <marcelo@dwitmee.com>
2015-02-02 21:07:12 +0000

    handling empty/error responses on locations

--- therewhere/therewhere/Friends.swift
+++ therewhere/therewhere/Friends.swift
@@ -7,6 +7,37 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
 class Friends: NSObject {
     var phoneNumberArray : [(String, Int)] = []
+    
+    func getLocation(friendID : String){
+        let url = APIConnectionManager.serverAddress+"/api/v1/users/"+friendID+"/location"
+
+        Alamofire.request(.GET, url)
+            .responseJSON { (req, res, json, error) in
+                if(error != nil) {
+                    NSLog("Error: \(error)")
+                    println(req)
+                    println(res)
+                }
+                else {
+                    NSLog("REST: getlocation for id: \(friendID)")
+                    var json = JSON(json!)
+
+                    if json["x"]&&json["y"] {
+                        var latitude :Double? = json["y"].double!
+                        var longitude :Double? = json["x"].double!
+                        var coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                    }else{
+                        println("no location available! sorry mate ")
+                    }
+
+                }
+        }
+
+    }
+    
 }
