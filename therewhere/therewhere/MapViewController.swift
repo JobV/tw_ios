232b436e849207cab762f48b27a9f362d7d3690f
Marcelo Lebre <marcelo@dwitmee.com>
2015-04-22 17:27:00 +0100

    adding fancy buttons

--- therewhere/therewhere/MapViewController.swift
+++ therewhere/therewhere/MapViewController.swift
@@ -31,6 +31,45 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     var userPin = CustomPointAnnotation()
     var userProfile = UserProfile.sharedInstance
     
+    @IBOutlet var optionsButton: UIButton!
+    @IBAction func optionsButton(sender: AnyObject) {
+        // 1
+        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .ActionSheet)
+        
+        // 2
+        let deleteAction = UIAlertAction(title: "Delete", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("File Deleted")
+        })
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("File Saved")
+        })
+        
+        //
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("Cancelled")
+        })
+        
+        
+        // 4
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(saveAction)
+        optionMenu.addAction(cancelAction)
+        
+        // 5
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+    }
+
+    @IBOutlet var userProfilePictureButton: UIButton!
+    @IBAction func userProfilePictureButton(sender: AnyObject) {
+    }
+    
+    @IBOutlet var friendProfilePictureButton: UIButton!
+    @IBAction func friendProfilePictureButton(sender: AnyObject) {
+    }
+    
     var friendLocation = CLLocationCoordinate2D(latitude: 0,longitude: 0){
         didSet {
             if (firstTimeOpeningMap){
