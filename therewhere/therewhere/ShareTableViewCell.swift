3af30827f7f1c001a3ffdbb206a134fc019d884e
Marcelo Lebre <marcelo@dwitmee.com>
2015-04-24 17:04:41 +0100

    installing FBSDKSharekit

---
+++ therewhere/therewhere/ShareTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShareTableViewCell.swift
+//  therewhere
+//
+//  Created by Marcelo Lebre on 24/04/15.
+//  Copyright (c) 2015 therewhereinc. All rights reserved.
+//
+
+import UIKit
+
+class ShareTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
